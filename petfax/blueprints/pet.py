195a2c63dd1df18770457025c38179839e07fb32
CoderKaiHan <hankaicoder@gmail.com>
2024-05-09 14:22:47 -0400

    Created more views

--- petfax/blueprints/pet.py
+++ petfax/blueprints/pet.py
@@ -13,12 +13,22 @@ bp = Blueprint(
     url_prefix='/pets'
     )
 
-#index route
+#index route, show all pets(Read)
 @bp.route('/')
 def index():
     return render_template('index.html', pets = pets)
 
-#test route
-@bp.route('/test')
-def test():
-    return 'This is a test route!'
+#Add a new pet route(Create)
+@bp.route('/new')
+def new():
+    return render_template('new_pet.html')
+
+#Show a pet route(Read)
+@bp.route('/<int:pet_id>')
+def show(pet_id):
+    return render_template('show_pet.html', pet = pets[pet_id])
+
+#Create a new facts for a pet
+@bp.route('/new_fun_fact')
+def new_fun_fact():
+    return render_template('new_fun_fact.html')
