9f80f769b4a8cb51ec550395ee9ca6138fcb22a7
CoderKaiHan <hankaicoder@gmail.com>
2024-05-11 21:13:27 -0400

    Insert a new_fact data into database

--- petfax/blueprints/fact.py
+++ petfax/blueprints/fact.py
@@ -1,5 +1,7 @@
 from flask import ( Blueprint, render_template, request, redirect )
 import json
+#import models
+from ..models import models
 
 pets = json.load(open('pets.json'))
 print(pets)
@@ -17,7 +19,17 @@ bp = Blueprint(
 @bp.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-      print (request.form)
+
+      #set the variables with the form data  received
+      submitter = request.form['submitter']
+      fact = request.form['fun_fact']
+      #create a new fact instance from Fact model
+      new_fact = models.Fact(submitter = submitter, fact = fact)
+      #add new_fact to the Flask-SQLAchemy database session
+      models.db.session.add(new_fact)
+      #Commit the database session(insert into database)
+      models.db.session.commit()
+      
       return redirect('/facts')
     
     return render_template('facts/index.html', pets = pets)
